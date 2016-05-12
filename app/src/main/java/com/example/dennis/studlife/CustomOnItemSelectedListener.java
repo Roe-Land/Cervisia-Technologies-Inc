0831b48ecaa0a6a7a7ee2d655be060d386d8a02a
richardvanginkel <richardvanginkel@live.nl>
2016-05-12 12:44:20 +0200

    XML activity main aangepast

---
+++ app/src/main/java/com/example/dennis/studlife/CustomOnItemSelectedListener.java
@@ -0,0 +1,44 @@
+package com.example.dennis.studlife;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+
+/**
+ * Created by dennis on 12-5-2016.
+ */
+public class CustomOnItemSelectedListener implements AdapterView.OnItemSelectedListener {
+    private Class from;
+
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Spinner spinner = (Spinner) parent;
+
+
+        if((spinner.getId() == R.id.spinner) & ( (Spinner) (spinner.getId()).getItemIdAtPosition(position).toString() == "Uit") ){
+        //Test nieuwe branch hallo
+        }
+
+        switch(spinner.getId()){
+            case (R.id.spinner & spinner.getItemIdAtPosition(position).toString == "Huis"): {
+                if (){
+
+                }
+            }
+            case "Uitgaan" : {
+
+            }
+            case "College" : {
+
+            }
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
