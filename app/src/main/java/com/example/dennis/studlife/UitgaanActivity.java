4dd0ea27a54c98c4f946369240c22cf2524501cc
denniskleverwal <dennis.kleverwal7@gmail.com>
2016-05-17 16:18:12 +0200

    Voor roeland

--- app/src/main/java/com/example/dennis/studlife/UitgaanActivity.java
+++ app/src/main/java/com/example/dennis/studlife/UitgaanActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class UitgaanActivity extends AppCompatActivity {
 
@@ -12,17 +13,10 @@ public class UitgaanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uitgaan);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner3);
+        spinner.setOnItemSelectedListener(new CustomOnItemSelectedListener(this));
     }
 
-    public void goHome(){
-        Intent intent = new Intent(this, ThuisActivity.class);
-        startActivity(intent);
-    }
-
-    public void goCollege(){
-        Intent intent = new Intent(this, CollegeActivity.class);
-        startActivity(intent);
-    }
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
